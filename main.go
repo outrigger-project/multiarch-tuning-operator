6388bc1c484386200b5ccd35796101bb105bbc86
AnnaZivkovic <tzivkovi@redhat.com>
2024-06-17 18:11:25 -0700

    MULTIARCH-4545: Added the webhooks

--- main.go
+++ main.go
@@ -160,6 +160,8 @@ func RunOperator(mgr ctrl.Manager) {
 			APIVersion: gvk.GroupVersion().String(),
 		}),
 	}).SetupWithManager(mgr), unableToCreateController, controllerKey, "ClusterPodPlacementConfig")
+	must((&multiarchv1alpha1.ClusterPodPlacementConfig{}).SetupWebhookWithManager(mgr), unableToCreateController,
+		controllerKey, "ClusterPodPlacementConfigConversionWebhook")
 }
 
 func RunClusterPodPlacementConfigOperandControllers(mgr ctrl.Manager) {
